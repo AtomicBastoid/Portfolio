2a3a78b5e550ab9bd403b52d192662bf0b896e66
Eishal Keshwani <keshwanieishal001@gmail.com>
2025-03-18 14:26:28 +0500

    Fixed section overlapping

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // For Loop Syntax
 // for (var i=0; i < 10; i++) {
 //  condition
@@ -15,22 +14,4 @@ document.addEventListener ("scroll", () => {
     } else {
         header.classList.remove("scrolled")
     }
-=======
-// For Loop Syntax
-// for (var i=0; i < 10; i++) {
-//  condition
-// }
-
-
-// Navbar Scroll Change Script
-
-document.addEventListener ("scroll", () => {
-    const header = document.querySelector("header")
-
-    if (window.scrollY > 0) {
-        header.classList.add("scrolled")
-    } else {
-        header.classList.remove("scrolled")
-    }
->>>>>>> 1aa38e500e745a221b0258daf500baa774136509
 })
