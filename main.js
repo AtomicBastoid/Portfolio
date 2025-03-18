937de3a6c8a21384b33d20a15b048791832dd6bb
Eishal Keshwani <keshwanieishal001@gmail.com>
2025-03-19 01:50:58 +0500

    Updated Services Section

--- main.js
+++ main.js
@@ -15,3 +15,39 @@ document.addEventListener ("scroll", () => {
         header.classList.remove("scrolled")
     }
 })
+
+// Services Section Popup Script
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".Card");
+    const popups = document.querySelectorAll(".popup");
+    const overlay = document.querySelector("#overlay");
+
+    cards.forEach(card => {
+        card.addEventListener("click", () => {
+            const popupId = card.id.replace("-card", "-popup");
+            const currentPopup = document.querySelector("#" + popupId);
+            if (currentPopup) {
+                currentPopup.classList.add("active");
+                overlay.classList.add("active");
+            }
+        });
+        
+    });
+
+    popups.forEach(popup => {
+        const close = popup.querySelector(".close-button");
+        close.addEventListener("click", () => {
+            popup.classList.remove("active");
+            overlay.classList.remove("active");
+        });
+    }
+    );
+
+    overlay.addEventListener("click", () => {
+        popups.forEach(popup => {
+            popup.classList.remove("active");
+        });
+        overlay.classList.remove("active");
+    });
+});
